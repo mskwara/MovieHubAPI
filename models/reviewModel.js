327b84369037b08cd9c8f024879cae2b85b891b0
lstachon <64076091+lstachon@users.noreply.github.com>
2021-04-07 18:40:07 +0200

    prettier

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema({
-  review_content: {
-    type: String,
-  },
-  FilmID: {
-    type: int,
-    required: [true, "Comment has to belong to the movie"],
-  },
+    review_content: {
+        type: String,
+    },
+    FilmID: {
+        type: int,
+        required: [true, "Comment has to belong to the movie"],
+    },
 });
 
 const Movie = mongoose.model("Movie", movieSchema);
--- models/userModel.js
+++ models/userModel.js
@@ -1,23 +1,23 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "User must have username."],
-  },
-  description: {
-    type: String,
-  },
-  password: {
-    type: String,
-    required: [true, "User must have a password"],
-  },
-  ratedMovies: [
-    {
-      type: mongoose.Schema.ObjectId,
-      ref: "Movie",
+    username: {
+        type: String,
+        required: [true, "User must have username."],
     },
-  ],
+    description: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: [true, "User must have a password"],
+    },
+    ratedMovies: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: "Movie",
+        },
+    ],
 });
 
 const User = mongoose.model("User", userSchema);
