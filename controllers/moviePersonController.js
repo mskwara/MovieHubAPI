1abc04ac7d2f48a4d182877dcbdd91e788fa3dfa
Adam <adamniemiec862@gmail.com>
2021-05-18 17:27:31 +0200

    moviePerson CRUD added

---
+++ controllers/moviePersonController.js
@@ -0,0 +1,86 @@
+const MoviePerson = require("../models/moviePersonModel");
+
+exports.createMoviePerson = async (req, res, next) => {
+    try {
+        const person = await MoviePerson.create(req.body);
+
+        res.status(201).json({
+            status: "success",
+            person,
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+exports.getAllPersons = async (req, res, next) => {
+    try {
+        const persons = await MoviePerson.find();
+
+        res.status(200).json({
+            status: "success",
+            results: persons.length,
+            persons,
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+exports.getMoviePerson = async (req, res, next) => {
+    try {
+        const person = await MoviePerson.findById(req.params.moviePersonID);
+
+        res.status(200).json({
+            status: "success",
+            person,
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+exports.updatePerson = async (req, res, next) => {
+    try {
+        const person = await MoviePerson.findByIdAndUpdate(
+            req.params.moviePersonID,
+            req.body,
+            { new: true }
+        );
+
+        res.status(200).json({
+            status: "success",
+            person,
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+exports.deleteMovie = async (req, res, next) => {
+    try {
+        await MoviePerson.findByIdAndDelete(req.params.moviePersonID);
+
+        res.status(204).json({
+            status: "success",
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+exports.getPersonsByRole = async (req, res, next) => {
+    try {
+        const persons = await MoviePerson.find(
+            {role: req.params.role}
+        );
+
+        res.status(200).json({
+            status: "success",
+            results: persons.length,
+            persons,
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
---
+++ routers/moviePersonRouter.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const movieController = require("../controllers/moviePersonController");
+
+const router = express.Router();
+
+router
+    .route("/")
+    .post(movieController.createMoviePerson)
+    .get(movieController.getAllPersons);
+
+router
+    .route("/:moviePersonID")
+    .get(movieController.getMoviePerson)
+    .patch(movieController.updateMoviePerson)
+    .delete(movieController.deleteMoviePerson);
+
+router
+    .route("/MoviePerson/role/:role")
+    .get(movieController.getPersonsByRole)
+
+//router.route("/top/:quantity").get(movieController.getTopMovies);
+
+module.exports = router;
